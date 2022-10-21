1f02713d59d735b0b76b683c267b1081c264df22
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-10-21 18:03:07 +0300

    Added 2 modal for delete and add data

--- PasswordManager/ClientApp/src/components/Account/Modal_delete.js
+++ PasswordManager/ClientApp/src/components/Account/Modal_delete.js
@@ -1,18 +1,17 @@
 import React, { useState,useRef } from 'react';
 import style from './Account.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import add_icon from './add_icon.png';
+import delete_icon from './delete_icon.png';
 
-export default function Modal_password() {
+export default function Modal_delete() {
 
     const [message, setMessage] = useState('');
 
     const refName_record = useRef("");
-    const refPassword = useRef("");
-    const refDescription = useRef("");
+    
     
 
-    const storepassword = () => {
+    const deletepassword = () => {
 
         function getCookie(cname) {
             let name = cname + "=";
@@ -37,20 +36,20 @@ export default function Modal_password() {
             body: JSON.stringify({
                 "Username": getCookie("username"),
                 "Name": refName_record.current.value,
-                "Password": refPassword.current.value,
-                "Description": refDescription.current.value
+                "Password":"null"
+                
             })
         };
 
         //call api from backend and send json data,which create before
 
-        fetch('http://localhost:32349/api/add_data', requestOptions)
+        fetch('http://localhost:32349/api/deletedatastore', requestOptions)
             .then(response => response.json())
             .then((responseData) => {
 
                 if (responseData == "Succes") {
 
-                    setMessage("Data added successfully");
+                    setMessage("Data was deleted successfully");
 
                     sessionStorage.setItem("windows", "active");
 
@@ -72,32 +71,28 @@ export default function Modal_password() {
 
             <body>
 
-                <button className={style.button_add} role="button" data-toggle="modal" data-target="#add_password"  >
-                    <img src={add_icon} className={style.add_image} /> <span class="text">Add password</span></button>
+                <button className={style.button_delete} role="button" data-toggle="modal" data-target="#delete_password" >
+                    <img src={delete_icon} className={style.add_image} /> <span class="text">Delete data</span></button>
 
-                <div class="modal fade" id="add_password" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="delete_password" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Add Password</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Delete data</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <p> Title :</p>
+                                <p> Name :</p>
                                 <input type="text" ref={refName_record } class="form-control" style={{ width: "40%" }} required />
                                 <br />
-                                <p>Password :</p>
-                                <input type="password" ref={refPassword} class="form-control" style={{ width: "40%" }} required />
-                                <br />
-                                <p>Description :</p>
-                                <input type="text" ref={refDescription} class="form-control" style={{ width: "40%" }} />
+                               
                             </div>
                             <p>{message}</p>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary" onClick={storepassword}>Save changes</button>
+                                <button type="button" class="btn btn-primary" onClick={deletepassword}>Save changes</button>
                             </div>
                         </div>
                     </div>
