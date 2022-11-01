9f702e2dcbf4b60212fcf40bdaa8af6245e8b09e
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-11-01 21:49:51 +0200

    Added modal where user can change secret question and answer and added css for this

---
+++ PasswordManager/ClientApp/src/components/Settings/Modal_edit_secret_question.js
@@ -0,0 +1,131 @@
+import React, { useState,useRef } from 'react';
+import style from './Modal_edit_secret_question.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import edit_icon from './edit_icon.png';
+
+export default function Modal_edit_secret_question(props) {
+
+    const [message, setMessage] = useState('');
+   
+
+    const refanswer = useRef("");
+    const refnewQuestion = useRef("");
+    const refnewAnswer = useRef("");
+
+
+        function getCookie(cname) {
+            let name = cname + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+
+    const editquestion = () => {
+
+        setMessage("Please wait");
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "Username": getCookie("username"),
+                "NewQuestion": refnewQuestion.current.value,
+                "NewAnswer": refnewAnswer.current.value,
+                "OldAnswer": refanswer.current.value
+                
+            })
+        };
+
+        //call api from backend and send json data,which create before
+
+        fetch('http://localhost:32349/api/edit_secret_question', requestOptions)
+            .then(response => response.json())
+            .then((responseData) => {
+
+                if (responseData == "Succes") {
+
+                    setMessage("Secret question and answer has change successfully");
+
+                    var field = document.getElementById("oldanswer");
+                    field.value = "";
+
+                    field = document.getElementById("newquestion");
+                    field.value = "";
+
+                    field = document.getElementById("newanswer");
+                    field.value = "";
+                   
+                }
+
+                else
+                    setMessage(responseData);
+
+
+            });
+    }
+
+
+
+
+
+        return (
+
+            <body>
+
+
+                <img src={edit_icon} className={ style.edit_icon} data-toggle="modal" data-target="#edit_secret_question" />
+
+                <div class="modal fade" id="edit_secret_question" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit secret question</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+
+                                <p> Actual secret question: {props.question}</p>
+
+                                <br />
+                                <p> Answer:</p>
+                                <input type="text" id="oldanswer" ref={refanswer} class="form-control" style={{ width: "70%" }} required />
+
+
+                                <br />
+                                <p> New secret question:</p>
+                                <input type="text" ref={refnewQuestion} id="newquestion" class="form-control" style={{ width: "70%" }} required />
+
+                                <br />
+                                <p> New answer for new question:</p>
+                                <input type="text" ref={refnewAnswer} id="newanswer" class="form-control" style={{ width: "70%" }} required />
+
+                            </div>
+                            <p>{message}</p>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" onClick={editquestion}>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </body>
+        );
+
+
+    
+}
+
+
+
+
