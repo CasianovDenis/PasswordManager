0a0754bde6b33df0915ee76c14e11b3a32980040
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-12-01 19:23:34 +0200

    Create new class for list,which use when call api getquestion

---
+++ PasswordManager/Models/TimeText_list.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Models
+{
+    public class TimeText_list
+    {
+        public string Text { get; set; }
+
+        public string Time { get; set; }
+
+       public TimeText_list(string text,string time)
+        {
+            Text = text;
+            Time = time;
+        }
+    }
+}
