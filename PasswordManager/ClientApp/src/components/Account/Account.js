3b8bee5528f8fecb219c2dc92bec9d7f3bad0aa6
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-12-13 17:05:32 +0200

    Fix bug adding try catch

--- PasswordManager/ClientApp/src/components/Account/Account.js
+++ PasswordManager/ClientApp/src/components/Account/Account.js
@@ -24,107 +24,116 @@ export default function Account() {
     if (GetCookie("status_account") != "online") redirect.push('/');
 
    
-    
-    useEffect(() => {
+   
+        useEffect(() => {
 
-        //added loading animation,timeout 5 second,after animation are hidden
-        setTimeout(() => {
+            //added loading animation,timeout 5 second,after animation are hidden
+            setTimeout(() => {
 
-            const animation = document.getElementsByClassName('gifloading')[0];
-            const loading_div = document.getElementsByClassName('loader_wrapper')[0];
-            
+                //catch exception
+                try {
+                const animation = document.getElementsByClassName('gifloading')[0];
+                const loading_div = document.getElementsByClassName('loader_wrapper')[0];
+
                 animation.style.minWidth = "0px";
                 animation.style.height = "0px";
 
                 loading_div.style.minWidth = "0px";
                 loading_div.style.height = "0px";
 
-        
-        }, 5000)
+            }
+                catch (event) {
 
-        //create request obj
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
+                    console.log(event);
+               
+            }
 
-                "Username": GetCookie("username"),
-                "Name": "null",
-                "Password": "null"
+            }, 5000)
 
-            })
-        };
-
-       //called api for get user data and display him
-            fetch('http://localhost:32349/api/getdatastore', requestOptions)
-            .then(response => response.json())
-                .then((responseData) => {
-                setDbData(responseData)
-                
-            });
-
-    }, [dbdata]);
-
-
-    
-    const delete_password = (ev) => {
-
-        let item = ev.target.getAttribute('title');
-
-        if (window.confirm("You want to delete data: " + item) == true) {
-
+            //create request obj
             const requestOptions = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
+
                     "Username": GetCookie("username"),
-                    "Name": item,
+                    "Name": "null",
                     "Password": "null"
 
                 })
             };
 
-            //called api where send json request for delete data from DB
-
-            fetch('http://localhost:32349/api/deletedatastore', requestOptions)
+            //called api for get user data and display him
+            fetch('http://localhost:32349/api/getdatastore', requestOptions)
                 .then(response => response.json())
                 .then((responseData) => {
-
-                    if (responseData != "Succes") alert("Error : data can not deleted");
+                    setDbData(responseData)
 
                 });
+
+        }, [dbdata]);
+    
+
+
+
+        const delete_password = (ev) => {
+
+            let item = ev.target.getAttribute('title');
+
+            if (window.confirm("You want to delete data: " + item) == true) {
+
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        "Username": GetCookie("username"),
+                        "Name": item,
+                        "Password": "null"
+
+                    })
+                };
+
+                //called api where send json request for delete data from DB
+
+                fetch('http://localhost:32349/api/deletedatastore', requestOptions)
+                    .then(response => response.json())
+                    .then((responseData) => {
+
+                        if (responseData != "Succes") alert("Error : data can not deleted");
+
+                    });
+            }
+
         }
-            
-    }
 
-    const copy = (ev) => {
+        const copy = (ev) => {
 
-        let item = ev.target.getAttribute('alt');
-        let password;
-        for (let index = 0; index < dbdata.length; index++)
-            if (dbdata[index].Name == item) password = dbdata[index].Password;
-       
+            let item = ev.target.getAttribute('alt');
+            let password;
+            for (let index = 0; index < dbdata.length; index++)
+                if (dbdata[index].Name == item) password = dbdata[index].Password;
 
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(password);
 
-        // Alert the copied text
-        var elem = document.getElementById("popup_div");
-        elem.style.display = "block";
-        elem = document.getElementById("popup_text");
-        elem.style.display = "block";
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(password);
 
-        setTimeout(() => {
-
-            elem = document.getElementById("popup_div");
-            elem.style.display = "none";
+            // Alert the copied text
+            var elem = document.getElementById("popup_div");
+            elem.style.display = "block";
             elem = document.getElementById("popup_text");
-            elem.style.display = "none";
+            elem.style.display = "block";
+
+            setTimeout(() => {
+
+                elem = document.getElementById("popup_div");
+                elem.style.display = "none";
+                elem = document.getElementById("popup_text");
+                elem.style.display = "none";
 
 
-        }, 5000)
-    }
-
+            }, 5000)
+        }
+    
     return (
 
        
