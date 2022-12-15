3bd2214b5c83c9c9672fae001c2ee2718a384e8d
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-12-15 22:16:06 +0200

    test

---
+++ PasswordManager/ClientApp/src/components/tests/Firsttest.test.js
@@ -0,0 +1,11 @@
+﻿import { render, screen, cleanup } from "@testing-library/react";
+import Text from "./Text";
+import "@testing-library/jest-dom";
+
+test("should render text component", () => {
+
+    render(<Text />);
+    var textElem = screen.getByTestId("text");
+    expect(textElem).toBeInTheDocument();
+
+});
---
+++ PasswordManager/ClientApp/src/components/tests/Text.js
@@ -0,0 +1,25 @@
+﻿import React, { useState, useRef, useEffect } from 'react';
+import { } from 'react-router-dom';
+
+
+
+export default function Text() {
+    
+
+
+
+
+   
+
+    return (
+        <div>
+
+            <input type="text" id="text" />
+            </div>
+        
+
+
+
+    );
+
+}
