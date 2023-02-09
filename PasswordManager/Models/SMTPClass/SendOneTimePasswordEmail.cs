95ff0e517615db804f75b5e6ce94e7aa2c2aa143
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-09 19:31:06 +0200

    Was hid real data

--- PasswordManager/Models/SMTPClass/SendOneTimePasswordEmail.cs
+++ PasswordManager/Models/SMTPClass/SendOneTimePasswordEmail.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
 
 namespace PasswordManager.Models
 {
     public class SendOneTimePasswordEmail
     {
-        public SendOneTimePasswordEmail(string Password,string Email)
+        public SendOneTimePasswordEmail(string Password, string Email)
         {
             //Send email which contain crypted password
             SmtpClient Smtp = new SmtpClient("smtp.mail.ru", 587);
             Smtp.EnableSsl = true;
-            Smtp.Credentials = new NetworkCredential("kasyanov_2001@mail.ru", "VnpsqpHSY8pysJn7Lmum");//real email and password
-                                                                                                      //was hide
+            Smtp.Credentials = new NetworkCredential("email@mail.ru", "password");//real email and password
+                                                                                  //was hide
             MailMessage Message = new MailMessage();
-            Message.From = new MailAddress("kasyanov_2001@mail.ru");//real email was hide
+            Message.From = new MailAddress("email@mail.ru");//real email was hide
 
             Message.To.Add(new MailAddress(Email));
             Message.Subject = "one-time password";
