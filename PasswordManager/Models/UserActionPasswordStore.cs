3ae35ccb85777740d08e3466491623b185666818
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-22 22:14:21 +0200

    Created new class for get authorization token and data for save user passwords

---
+++ PasswordManager/Models/UserActionPasswordStore.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.Models
+{
+    public class UserActionPasswordStore : Password_store
+    {
+        [Required]
+        public string AuthorizationToken { get; set; }
+    }
+}
