4e01a050b3a54b4175f773eba4dd98271a0001ca
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-03-13 11:55:26 +0200

    Change nav menu import

--- PasswordManager/ClientApp/src/components/NavMenu/Layout.js
+++ PasswordManager/ClientApp/src/components/NavMenu/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import {NavMenu}  from './NavMenu';
+import NavMenu   from './NavMenu';
 
 export default function Layout(props) {
   
